d517971c22bbf8f1c1f3086a496325a214b6d91c
eichan <eichan216@gmail.com>
2024-02-04 17:32:18 +0900

    Vuetify >> initial setting

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,17 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
   transpileDependencies: true,
+
   chainWebpack: (config) => {
     config.plugin("define").tap((args) => {
-      args[0]["__VUE_PROD_HYDRATION_MISMATCH_DETAILS__"] = false;
-      return args;
-    });
+      args[0]["__VUE_PROD_HYDRATION_MISMATCH_DETAILS__"] = false
+      return args
+    })
   },
-});
+
+  pluginOptions: {
+    vuetify: {
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
+})
